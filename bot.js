09750793b718b12906e780e607cd6999e4086861
martinenti <40662211+martinenti@users.noreply.github.com>
2019-06-08 01:13:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -792,41 +792,5 @@ client.on('message', message => {
     }
 });
 
-client.on('message', message => {
-	var prefix = "+"
-  if (message.author.x5bz) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "kick") {
-               if(!message.channel.guild) return message.reply('** This command only for servers**');
-         
-  if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("**You Don't Have ` KICK_MEMBERS ` Permission**");
-  if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("**I Don't Have ` KICK_MEMBERS ` Permission**");
-  let user = message.mentions.users.first();
-  let reason = message.content.split(" ").slice(2).join(" ");
-  if (message.mentions.users.size < 1) return message.reply("**mention to the member**");
-  if(!reason) return message.reply ("**type the reason**");
-  if (!message.guild.member(user)
-  .kickable) return message.reply("**i cant kick this member**");
-
-  message.guild.member(user).kick();
-
-  const kickembed = new Discord.RichEmbed()
-  .setauthor(`KICKED!`, user.displayavatarURL)
-  .setColor("RaNDOM")
-  .setTimestamp()
-  .addField("**User:**",  '**[ ' + `${user.tag}` + ' ]**')
-  .addField("**By:**", '**[ ' + `${message.author.tag}` + ' ]**')
-  .addField("**Reason:**", '**[ ' + `${reason}` + ' ]**')
-  message.channel.send({
-    embed : kickembed
-  })
-}
-});
 
 client.login(process.env.BOT_TOKEN);
