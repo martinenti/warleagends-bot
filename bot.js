7e850a12b697a5c68161ffcb95edc7a9954578e5
martinenti <40662211+martinenti@users.noreply.github.com>
2019-06-07 07:18:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -52,5 +52,24 @@ client.user.setGame(`+help`,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
 });
+//امر عرض صورة السيرفر 
+client.on("message", message => {
+    const prefix = "+"
+              
+          if(!message.channel.guild) return;
+   if(message.author.bot) return;
+      if(message.content === prefix + "image"){ 
+          const embed = new Discord.RichEmbed()
+  
+      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
+  .setAuthor(message.author.username, message.guild.iconrURL)
+    .setColor(0x164fe3)
+    .setImage(message.guild.iconURL)
+    .setURL(message.guild.iconrURL)
+                    .setTimestamp()
+
+   message.channel.send({embed});
+      }
+  });
 
 client.login(process.env.BOT_TOKEN); 
