78e38fe39f1cdb570ee48a09a481fba7ed557061
martinenti <40662211+martinenti@users.noreply.github.com>
2019-06-07 17:28:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -471,8 +471,7 @@ client.on("message", message => {
     });
 
 
-const Discord = require("discord.js");
-const client = new Discord.Client();
+
 client.on('message',async message => {
   if(message.author.bot) return;
 var prefix = "+"
