2f6421837ce7f657396fb729330caa499ba44039
martinenti <40662211+martinenti@users.noreply.github.com>
2019-06-08 18:24:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -842,4 +842,14 @@ if(message.content.startsWith(prefix + 'set-say')) {
 })
 
 
+client.on('message',function(message) {
+	let prefix = "+";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(`**# ${args}**`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
+
 client.login(process.env.BOT_TOKEN);
